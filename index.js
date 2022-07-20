fe0f785257c1fc83d013a71cfc7f6b506a45e9d1
João Victor Nascimento <66145032+joaovictorjvn@users.noreply.github.com>
2022-07-20 17:46:08 -0300

    Entrega 1

---
+++ index.js
@@ -0,0 +1,3 @@
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu-lateral')
+botaoMenu.addEventListener('click', () => {menu.classList.toggle('menu-lateral--ativo')})
